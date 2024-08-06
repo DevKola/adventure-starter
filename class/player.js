d7918d8ce5a24d2d10d8fe222691a08c809cf011
DevKola <koladeayinde@gmail.com>
2024-08-06 20:19:23 +0100

    solutions

--- class/player.js
+++ class/player.js
@@ -1,56 +1,72 @@
+const { Food } = require("./food");
 class Player {
+  constructor(name, startingRoom, items) {
+    this.name = name;
+    this.currentRoom = startingRoom;
+    this.items = [];
+  }
 
-    constructor(name, startingRoom) {
-        this.name = name;
-        this.currentRoom = startingRoom;
-        this.items = [];
+  move(direction) {
+    const nextRoom = this.currentRoom.getRoomInDirection(direction);
+
+    // If the next room is valid, set the player to be in that room
+    if (nextRoom) {
+      this.currentRoom = nextRoom;
+
+      nextRoom.printRoom(this);
+    } else {
+      console.log("You cannot move in that direction");
     }
+  }
 
-    move(direction) {
-
-        const nextRoom = this.currentRoom.getRoomInDirection(direction);
-
-        // If the next room is valid, set the player to be in that room
-        if (nextRoom) {
-            this.currentRoom = nextRoom;
-
-            nextRoom.printRoom(this);
-        } else {
-            console.log("You cannot move in that direction");
-        }
+  printInventory() {
+    if (this.items.length === 0) {
+      console.log(`${this.name} is not carrying anything.`);
+    } else {
+      console.log(`${this.name} is carrying:`);
+      for (let i = 0; i < this.items.length; i++) {
+        console.log(`  ${this.items[i].name}`);
+      }
     }
+  }
 
-    printInventory() {
-        if (this.items.length === 0) {
-            console.log(`${this.name} is not carrying anything.`);
-        } else {
-            console.log(`${this.name} is carrying:`);
-            for (let i = 0 ; i < this.items.length ; i++) {
-                console.log(`  ${this.items[i].name}`);
-            }
-        }
-    }
+  takeItem(itemName) {
+    // Fill this in
+    const [wantedItem] = this.currentRoom.items.filter(
+      (item) => item.name === itemName
+    );
 
-    takeItem(itemName) {
+    this.items.push(wantedItem);
 
-        // Fill this in
+    this.currentRoom.items.splice(
+      this.currentRoom.items.indexOf(wantedItem),
+      1
+    );
+  }
 
-    }
+  dropItem(itemName) {
+    // Fill this in
+    if (this.items.length === 0) return;
 
-    dropItem(itemName) {
+    const [wantedItem] = this.items.filter((item) => item.name === itemName);
 
-        // Fill this in
-    }
+    this.currentRoom.items.push(wantedItem);
 
-    eatItem(itemName) {
-        // Fill this in
+    this.items.splice(this.items.indexOf(wantedItem), 1);
+  }
 
-    }
+  eatItem(itemName) {
+    // Fill this in
+    const [wantedItem] = this.items.filter((item) => item.name === itemName);
+    if (wantedItem instanceof Food)
+      this.items.splice(this.items.indexOf(wantedItem), 1);
+  }
 
-    getItemByName(name) {
-
-        // Fill this in
-    }
+  getItemByName(name) {
+    // Fill this in
+    const [newItem] = this.items.filter((item) => item.name === name);
+    return newItem;
+  }
 }
 
 module.exports = {
